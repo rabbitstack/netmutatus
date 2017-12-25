448d0465030b1aaedd0f57454ecbdbd70f614e5d
rabbitstack <rabbitstack7@gmail.com>
2017-12-25 13:28:32 +0100

    adding veth and peer addresses

--- lib/netmutatus/veth.rb
+++ lib/netmutatus/veth.rb
@@ -34,35 +34,95 @@ module Netmutatus
   class Veth
     include Netlink
 
+    attr_reader :veth_name, :peer_name
+
     # Creates a new veth network interface and its corresponding peer link.
     #
     # @param [String] veth_name veth interface name
     # @param [String] peer_name peer interface name
-    def initialize(veth_name, peer_name)
-      Netlink.do_in_netlink(Netlink::NETLINK_ROUTE) do |sock|
-        @veth = rtnl_link_veth_alloc
+    # @param [bool] force determines if veth links are created if not found in the cache
+    def initialize(veth_name, peer_name, force=true)
+      @cache = Netlink.alloc_cache
+      @veth_name = veth_name
+      @peer_name = peer_name
+      @veth_addrs = {}
+      @peer_addrs = {}
 
-        if @veth.null?
-          raise Errors::VethError, 'Unable to allocate veth link'
+      @veth = rtnl_link_get_by_name(@cache, FFI::MemoryPointer.from_string(veth_name))
+      @peer = rtnl_link_get_by_name(@cache, FFI::MemoryPointer.from_string(peer_name))
+
+      if (@veth.null? && @peer.null?) && force
+        # no veth and peer links found, so we attempt to create them
+        Netlink.do_in_netlink(Netlink::NETLINK_ROUTE) do |sock|
+          @veth = rtnl_link_veth_alloc
+
+          if @veth.null?
+            raise Errors::VethError, 'Unable to allocate veth link'
+          end
+          @peer = rtnl_link_veth_get_peer(@veth)
+
+          # initialize interface names
+          rtnl_link_set_name(@veth, FFI::MemoryPointer.from_string(veth_name))
+          rtnl_link_set_name(@peer, FFI::MemoryPointer.from_string(peer_name))
+
+          status = rtnl_link_add(sock,
+                                 @veth,
+                                 Netlink::NLM_F_CREATE |
+                                 Netlink::NLM_F_EXCL |
+                                 Netlink::NLM_F_ACK)
+
+          if status < 0
+            raise Errors::VethError, "Unable to add veth pair #{veth_name} <-> #{peer_name}: #{Netlink.error(status)}"
+          end
+
+          rtnl_link_put(@peer)
+          rtnl_link_put(@veth)
+
+          # reallocate cache and retrieve the links
+          @cache = Netlink.alloc_cache
+          @veth = rtnl_link_get_by_name(@cache, FFI::MemoryPointer.from_string(veth_name))
+          @peer = rtnl_link_get_by_name(@cache, FFI::MemoryPointer.from_string(peer_name))
+          if @veth.null? || @peer.null?
+            raise Errors::VethError, "Failed to retrieve veth pair #{veth_name} <-> #{peer_name} from the cache"
+          end
         end
-        @peer = rtnl_link_veth_get_peer(@veth)
-
-        # initialize interface names
-        rtnl_link_set_name(@veth, FFI::MemoryPointer.from_string(veth_name))
-        rtnl_link_set_name(@peer, FFI::MemoryPointer.from_string(peer_name))
-
-        status = rtnl_link_add(sock, @veth,
-                               Netlink::NLM_F_CREATE |
-                               Netlink::NLM_F_EXCL |
-                               Netlink::NLM_F_ACK)
-        if status < 0
-          raise Errors::VethError, "Unable to add veth pair #{veth_name} <-> #{peer_name}: #{Netlink.error(status)}"
-        end
-        rtnl_link_put(@peer)
-        rtnl_link_put(@veth)
       end
     end
 
-  end
+    # Adds an ip address for veth interface.
+    #
+    # @param [String] ip the ip address in ipv4 or ipv6 format
+    def add_veth_address(ip)
+      if @veth_addrs.key?(ip)
+        return
+      end
+      @veth_addrs[ip] = Netmutatus::Addr.new(self, ip).add
+    end
 
+    # Adds an ip address for peer interface.
+    #
+    # @param [String] ip the ip address in ipv4 or ipv6 format
+    def add_peer_address(ip)
+      if @peer_addrs.key?(ip)
+        return
+      end
+      @peer_addrs[ip] = Netmutatus::Addr.new(self, ip, veth_peer=true).add
+    end
+
+    def veth_index
+      rtnl_link_get_ifindex(@veth)
+    end
+
+    def peer_index
+      rtnl_link_get_ifindex(@peer)
+    end
+
+    def raw_veth
+      @veth
+    end
+
+    def raw_peer
+      @peer
+    end
+  end
 end
